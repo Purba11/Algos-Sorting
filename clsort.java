cd9c65c854fffa7ac7a3ff93328d402a52957328
Developer-Purba <purbachakraborty11@gmail.com>
2018-09-13 05:11:22 -0700

    Update clsort.java

--- clsort.java
+++ clsort.java
@@ -15,10 +15,7 @@ public static void main(String args[])
 	for(i=0;i<num;i++)
 	  	ar[i]=s.nextInt();	
 	System.out.println("Sorting:\n");
-	//bubble_sort(ar,num);
-	//selection_sort(ar,num);
-	//insertion_sort(ar,num);
-	    
+		    
     char choice;
     
     do
@@ -58,40 +55,31 @@ public static void main(String args[])
 
 static void bubble_sort(int a[],int n)
 {
-	/*Bubble sort is based on the idea of repeatedly comparing pairs 
-	of adjacent elements and then swapping their positions if they 
-	exist in the wrong order.
-	The complexity of bubble sort is O(n**2) in both worst and average 
-	cases, because the entire array needs to be iterated for every 
-	element.*/
+/*Bubble sort is based on the idea of repeatedly comparing pairs of adjacent elements and then swapping their positions if they 
+exist in the wrong order.The complexity of bubble sort is O(n**2) in both worst and average cases, because the entire array needs to be iterated for every 
+element.*/
 	
-	int t;
-	for(int k=0;k<n-1;k++){
-		for(int i=0;i<n-k-1;i++){ 
+int t;
+for(int k=0;k<n-1;k++){
+	for(int i=0;i<n-k-1;i++){ 
 			// (n-k-1) is for ignoring comparisons of elements which have already been compared in earlier iterations
-			if(a[i]>a[i+1]){
+  		if(a[i]>a[i+1]){
 				t=a[i];		//swapping of positions
 				a[i]=a[i+1];
 				a[i+1]=t;
-			}
 		}
 	}
+}
 }
 
 static void selection_sort(int a[],int n)
 {
-/*The Selection sort algorithm is based on the idea of finding the 
- minimum or maximum element in an unsorted array and then putting
- it in its correct position in a sorted array.Assume that the array 
- A=[7,5,4,2] needs to be sorted in ascending order.	The minimum 
- element in the array i.e. 2 is searched for and then swapped with 
- the element that is currently located at the first position, i.e. 7.
- Now the minimum element in the remaining unsorted array is searched 
- for and put in the second position, and so on.
- Time Complexity:to find the minimum element from the array of N elements,
- N-1 comparisons and then N-2 comparisons are required to find the minimum
- in the unsorted array. Therefore (N-1)+(N-2)+.....+1=(N.(N-1))/2 
- comparisons and N swaps result in overall complexity of O(N**2)   
+/*The Selection sort algorithm is based on the idea of finding the minimum or maximum element in an unsorted array and then putting
+ it in its correct position in a sorted array.Assume that the array A=[7,5,4,2] needs to be sorted in ascending order.	The minimum 
+ element in the array i.e. 2 is searched for and then swapped with the element that is currently located at the first position, i.e. 7.
+ Now the minimum element in the remaining unsorted array is searched for and put in the second position, and so on.
+ Time Complexity:to find the minimum element from the array of N elements, N-1 comparisons and then N-2 comparisons are required to find the minimum
+ in the unsorted array. Therefore (N-1)+(N-2)+.....+1=(N.(N-1))/2 comparisons and N swaps result in overall complexity of O(N**2)   
  */
 	
 	int min,t;
@@ -115,24 +103,12 @@ static void selection_sort(int a[],int n)
 
 static void insertion_sort(int a[],int n)
 {
-	/*Insertion sort is based on the idea that one element from the
-    input elements is consumed in each iteration to find its 
-	correct position i.e, the position to which it belongs 
-	in a sorted array.It iterates the input elements by growing 
-	the sorted array at each iteration. It compares the current
-	element with the largest value in the sorted array. If the 
-	current element is greater, then it leaves the element in 
-	its place and moves on to the next element else it finds 
-	its correct position in the sorted array and moves it to
-	that position. This is done by shifting all the elements, 
-	which are larger than the current element, in the sorted
-	array to one position ahead
-	Time Complexity:In worst case,each element is compared with 
-	all the other elements in the sorted array. For N elements, 
-	there will be N2 comparisons. Therefore, the time complexity 
-	is O(N**2)
-
-*/
+/*Insertion sort is based on the idea that one element from the input elements is consumed in each iteration to find its correct position i.e, the position to which it belongs 
+in a sorted array.It iterates the input elements by growing the sorted array at each iteration. It compares the current element with the largest value in the sorted array. If the 
+current element is greater, then it leaves the element in its place and moves on to the next element else it finds its correct position in the sorted array and moves it to
+that position. This is done by shifting all the elements, which are larger than the current element, in the sorted array to one position ahead
+Time Complexity:In worst case,each element is compared with all the other elements in the sorted array. For N elements, there will be N2 comparisons. Therefore, the time complexity 
+is O(N**2)*/
 	
 	for(int i=0;i<n;i++){
 		/*storing current element whose left side is checked for its 
